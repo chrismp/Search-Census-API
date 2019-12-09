99d95275048744b5be5990ea0fd06d5772cd2a36
Chris P <christopher.mark.persaud@gmail.com>
2019-12-09 13:55:09 -0500

    Update script

--- script.r
+++ script.r
@@ -1,11 +1,20 @@
 require(censusapi)
 
-Sys.getenv("CENSUS_API")
 View(listCensusApis())
 
 View(
   listCensusMetadata(
-    name = "acs/acs1",
+    name = "acs/acs5/cprofile",
     vintage = 2017
+  )
+)
+
+View(
+  getCensus(
+    name = "acs/acs5/cprofile",
+    vintage = 2017,
+    vars = c("CP02_2013_2017_067E"),
+    region = "county:*",
+    key = Sys.getenv("CENSUS_API")
   )
 )
